09090e5dda12cff388a421935ca44e7350f0a026
Eme005humberto <emersoncarpano3@gmail.com>
2023-11-01 16:32:43 -0600

    Subimos el Ecommerce

---
+++ Ecommerce.Repositorio/DBContext/DbEcommerceContext.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using Ecommerce.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Repositorio.DBContext;
+
+public partial class DbEcommerceContext : DbContext
+{
+    public DbEcommerceContext()
+    {
+    }
+
+    public DbEcommerceContext(DbContextOptions<DbEcommerceContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categoria { get; set; }
+
+    public virtual DbSet<DetalleVenta> DetalleVenta { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    public virtual DbSet<Venta> Venta { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.IdCategoria).HasName("PK__Categori__A3C02A10244E95D5");
+
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<DetalleVenta>(entity =>
+        {
+            entity.HasKey(e => e.IdDetalleVenta).HasName("PK__DetalleV__AAA5CEC21D836B81");
+
+            entity.Property(e => e.Total).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.IdProductoNavigation).WithMany(p => p.DetalleVenta)
+                .HasForeignKey(d => d.IdProducto)
+                .HasConstraintName("FK__DetalleVe__IdPro__5BE2A6F2");
+
+            entity.HasOne(d => d.IdVentaNavigation).WithMany(p => p.DetalleVenta)
+                .HasForeignKey(d => d.IdVenta)
+                .HasConstraintName("FK__DetalleVe__IdVen__5AEE82B9");
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("PK__Producto__098892105B862CA6");
+
+            entity.ToTable("Producto");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(1000)
+                .IsUnicode(false);
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Imagen).IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.PrecioOferta).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.IdCategoria)
+                .HasConstraintName("FK__Producto__IdCate__5070F446");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__5B65BF97AF1D0B90");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.NombreCompleto)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Rol)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Venta>(entity =>
+        {
+            entity.HasKey(e => e.IdVenta).HasName("PK__Venta__BC1240BD743C39D4");
+
+            entity.Property(e => e.FechaCreacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Total).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.Venta)
+                .HasForeignKey(d => d.IdUsuario)
+                .HasConstraintName("FK__Venta__IdUsuario__571DF1D5");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ dbEcommerce.sql
@@ -0,0 +1,49 @@
+--Base de datos para Ecommerce
+Create database DbEcommerce
+Use DbEcommerce
+
+--Creamos la tablas
+Create table Categoria(
+IdCategoria int primary key identity,
+Nombre Varchar(50),
+FechaCreacion datetime default getdate()
+)
+
+Create table Producto(
+IdProducto int primary key identity,
+Nombre Varchar(50),
+Descripcion Varchar(1000),
+IdCategoria int references Categoria(IdCategoria),
+Precio Decimal(10,2),
+PrecioOferta Decimal(10,2),
+Cantidad Int,
+Imagen Varchar(max),
+FechaCreacion datetime default getdate()
+)
+
+Create table Usuario(
+IdUsuario int primary key identity,
+NombreCompleto Varchar(50),
+Correo Varchar(50),
+Clave Varchar(50),
+Rol Varchar(50),--Administrador , Cliente
+FechaCreacion DateTime default getdate()
+)
+
+Create Table Venta(
+IdVenta int primary key identity,
+IdUsuario int references Usuario(IdUsuario),
+Total Decimal(10,2),
+FechaCreacion datetime default getdate()
+)
+
+Create table DetalleVenta
+(
+IdDetalleVenta int primary key identity,
+IdVenta int references Venta(IdVenta),
+IdProducto int references Producto(IdProducto),
+Cantidad int,
+Total Decimal(10,2)
+)
+Select * from Usuario
+Insert into Usuario(NombreCompleto,Correo,Clave,Rol)Values('administrador','admin@example.com','123','Administrador')
